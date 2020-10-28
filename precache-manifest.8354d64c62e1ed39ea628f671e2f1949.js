d6abc180ea0f909cecf75e233debccbeb06e1025
Michał Rozantsev <michal@MacBook-Pro-macbook-2.local>
2020-10-28 22:38:37 +0100

    Updates

---
+++ precache-manifest.8354d64c62e1ed39ea628f671e2f1949.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2671e312c2868dbf4df81912711f61c",
+    "url": "/Rozantsew/Netflix_Clone.git/index.html"
+  },
+  {
+    "revision": "3b9da94cee4f7629a9b0",
+    "url": "/Rozantsew/Netflix_Clone.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7ee93168af9080bb569f",
+    "url": "/Rozantsew/Netflix_Clone.git/static/js/2.c9865669.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Rozantsew/Netflix_Clone.git/static/js/2.c9865669.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b9da94cee4f7629a9b0",
+    "url": "/Rozantsew/Netflix_Clone.git/static/js/main.40f10ed9.chunk.js"
+  },
+  {
+    "revision": "aacbdc31e07708e6a9b8",
+    "url": "/Rozantsew/Netflix_Clone.git/static/js/runtime-main.c772e4cf.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Rozantsew/Netflix_Clone.git/precache-manifest.8354d64c62e1ed39ea628f671e2f1949.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Rozantsew/Netflix_Clone.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
