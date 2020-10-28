5524ff3d2a254f211e218142184debf9b4e88bf6
Michał Rozantsev <michal@MacBook-Pro-macbook-2.local>
2020-10-28 22:50:30 +0100

    Hash router added to the app.js

--- src/Components/banner/Banner.js
+++ src/Components/banner/Banner.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "../../axios";
 import requests from "../../requests";
 import { BannerContents, HeroBanner, HeroTitle } from "./Banner.styled";
+import { HashRouter } from "react-router-dom";
 
 const Banner = () => {
   const [movie, setMovie] = useState([]);
@@ -20,20 +21,22 @@ const Banner = () => {
   }, []);
 
   return (
-    <HeroBanner
-      style={{
-        backgroundSize: "cover",
-        backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie?.backdrop_path})`,
-        backgroundPosition: "center",
-      }}
-    >
-      <HeroTitle>
-        {movie?.title || movie?.name || movie?.original_name}
-      </HeroTitle>
-      <BannerContents></BannerContents>
-      {/*div width 2 buttons*/}
-      {/*description*/}
-    </HeroBanner>
+    <HashRouter>
+      <HeroBanner
+        style={{
+          backgroundSize: "cover",
+          backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie?.backdrop_path})`,
+          backgroundPosition: "center",
+        }}
+      >
+        <HeroTitle>
+          {movie?.title || movie?.name || movie?.original_name}
+        </HeroTitle>
+        <BannerContents></BannerContents>
+        {/*div width 2 buttons*/}
+        {/*description*/}
+      </HeroBanner>
+    </HashRouter>
   );
 };
 
